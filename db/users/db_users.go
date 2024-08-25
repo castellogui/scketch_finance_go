f25ab371d45c95189e1787574e6f5029fd280da7
Guilherme Castello <guilhermecastellobrasolin@gmail.com>
2024-08-25 00:27:46 -0300

    feat: add other fields in get user call

--- db/users/db_users.go
+++ db/users/db_users.go
@@ -3,15 +3,28 @@ package db_users
 import (
 	"context"
 
-	"scketch_finance_go/structs"
 	"scketch_finance_go/db"
+	"scketch_finance_go/structs"
 
 	"github.com/google/uuid"
 )
 
 func GetUser(db *db.Database, id uuid.UUID) (structs.User, error) {
 	var user structs.User
-	query := "SELECT id, name FROM users WHERE id = $1"
-	err := db.Pool.QueryRow(context.Background(), query, id).Scan(&user.Id, &user.Name)
+	query := "SELECT * FROM users WHERE id = $1"
+	err := db.Pool.QueryRow(context.Background(), query, id).Scan(
+		&user.Id,
+		&user.Name,
+		&user.SecondName,
+		&user.Email,
+		&user.Password,
+		&user.CreatedBy,
+		&user.CreatedAt,
+		&user.UpdatedBy,
+		&user.UpdatedAt,
+		&user.Deleted,
+		&user.DeletedBy,
+		&user.DeletedAt,
+	)
 	return user, err
 }
